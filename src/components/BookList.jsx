788c939ffa7b421d3a28f46327c80382e6d43e51
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-10-15 16:00:56 +0100

    app context

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,9 +3,14 @@ import "../App";
 import { API_URL } from "../API";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useAppContext } from "./context/appContext";
 
 const BookList = ({id, title}) => {
   const [books, setBooks] = useState([]);
+
+  const {favourites,addToFavourites,removeFromFavourites} = useAppContext();
+
+  console.log("favourites", favourites);
 
   useEffect(() => {
     axios
@@ -22,10 +27,10 @@ const BookList = ({id, title}) => {
       {books.map((book) => (
         <div key={book.id} className="book">
           <div>
-            <h2>{book.title}</h2>
+            <h4>{book.title}</h4>
           </div>
           <div><img src={book.image_url} alt="3"></img></div>
-          <div><button>Add to favourites</button></div>
+          <div><button onClick={() => addToFavourites(book)}>Add to favourites</button></div>
         </div>
       ))}
     </div>
