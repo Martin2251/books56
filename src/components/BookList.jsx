3a1b12b4a02d72067107b7e5c951e92ef8f0de4c
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-10-15 15:23:10 +0100

    css styling

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "../App";
-import { API_URL } from ''../API';
+import { API_URL } from "../API";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const BookList = () => {
+const BookList = ({id, title}) => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    axios.get(API_URL).then((res) => {
-      console.log(res.data)
-      setBooks(res.data)
-    }).catch(err => console.log(err));
-  },[]);
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  return <div className="book-list">BookList</div>;
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <div key={book.id} className="book">
+          <div>
+            <h2>{book.title}</h2>
+          </div>
+          <div><img src={book.image_url} alt="3"></img></div>
+          <div><button>Add to favourites</button></div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
